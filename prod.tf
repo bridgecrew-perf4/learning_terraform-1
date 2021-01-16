33ea9204cbf25cdecc05c49f8619661d96b2c8a3
raguirreg10 <romellaguirre@gmail.com>
2021-01-15 23:35:00 -0600

    added elastic ip to web instance

--- prod.tf
+++ prod.tf
@@ -11,9 +11,7 @@ resource "aws_s3_bucket" "prod_first_bucket" {
 }
 
 # Utilizing the default VPC in AWS
-resource "aws_default_vpc" "prod_vpc" {
-
-}
+resource "aws_default_vpc" "prod_vpc" {}
 
 # Creating the security group for our web server
 resource "aws_security_group" "prod_security_group" {
@@ -48,12 +46,21 @@ resource "aws_security_group" "prod_security_group" {
   }
 }
 
+# Creating an aws instance NGNIX
 resource "aws_instance" "prod_web" {
   ami                     = "ami-0592a8932e5a2fb0e"
   instance_type           = "t2.nano"
   vpc_security_group_ids  = [ aws_security_group.prod_security_group.id ]
   tags = {
     "Name" = "Terraform Nginx example"
+  }
+}
+
+# Creating Elastic IP
+resource "aws_eip" "prod_eip" {
+  instance = aws_instance.prod_web.id
+  tags = {
+    "elasticIP" = true
   }
 }
 
