2fef2e8fe901028804b2e647b8fa32d477324444
AguirreGomez, Romell <romell.aguirregomez@windriver.com>
2021-01-18 23:51:16 -0600

    adding autoscaling

--- prod.tf
+++ prod.tf
@@ -75,26 +75,26 @@ resource "aws_security_group" "prod_security_group" {
 
 # ====================================
 
-# Creating an aws instance NGNIX
-resource "aws_instance" "prod_web" {
-  count = 2
+# # Creating an aws instance NGNIX
+# resource "aws_instance" "prod_web" {
+#   count = 2
 
-  ami                     = "ami-0592a8932e5a2fb0e"
-  instance_type           = "t2.nano"
-  vpc_security_group_ids  = [ aws_security_group.prod_security_group.id ]
+#   ami                     = "ami-0592a8932e5a2fb0e"
+#   instance_type           = "t2.nano"
+#   vpc_security_group_ids  = [ aws_security_group.prod_security_group.id ]
 
-  tags = {
-    "Name" = "Terraform Nginx example"
-  }
-}
+#   tags = {
+#     "Name" = "Terraform Nginx example"
+#   }
+# }
 
-# ====================================
+# # ====================================
 
-# Creating Elastic IP Association
-resource "aws_eip_association" "prod_web_association" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_eip.id
-}
+# # Creating Elastic IP Association
+# resource "aws_eip_association" "prod_web_association" {
+#   instance_id   = aws_instance.prod_web.0.id
+#   allocation_id = aws_eip.prod_eip.id
+# }
 
 # ====================================
 
@@ -111,7 +111,7 @@ resource "aws_eip" "prod_eip" {
 
 resource "aws_elb" "prod_web_lb" {
   name            = "prod-web-lb"
-  instances       = aws_instance.prod_web.*.id
+  # instances       = aws_instance.prod_web.*.id
   subnets         = [ aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id ]
   security_groups = [ aws_security_group.prod_security_group.id ]
 
@@ -129,3 +129,40 @@ resource "aws_elb" "prod_web_lb" {
 
 # ====================================
 
+resource "aws_launch_template" "prod_web" {
+  name_prefix   = "prod-web"
+  image_id      = "ami-0592a8932e5a2fb0e"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "Launch Template"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+  availability_zones  = ["us-east-1d", "us-east-1b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Terraform"
+    value               = "true"
+    propagate_at_launch = true
+  }
+}
+
+# ====================================
+
+# Create a new load balancer attachment
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web_lb.id
+}
+
+# ====================================
